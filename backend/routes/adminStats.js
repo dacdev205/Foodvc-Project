87c65599e311e0574aa0673bdd0176712f5f1b45
dacdepchai <93912537+dacdepchai@users.noreply.github.com>
2024-02-28 20:20:56 +0700

    create adminStatsRoutes

--- backend/routes/adminStats.js
+++ backend/routes/adminStats.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
 const StatsAPI = require("../controllers/adminStatsControllers")
-//import modal
 router.get("/",StatsAPI.getAllDataForStats)
 module.exports = router

dc008ac4e8d1ce2aaf7b0734a71054fd18d19607
dacdepchai <93912537+dacdepchai@users.noreply.github.com>
2024-02-28 19:03:50 +0700

    add userControllers

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const fs = require("fs");
 module.exports = class usersAPI {
+  
   // get all users
     static async getAllUsers (req, res) {
         try {

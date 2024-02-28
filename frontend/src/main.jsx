88b2bd1bfd9c9b5e3657b83469304b58cbd2b762
dacdepchai <93912537+dacdepchai@users.noreply.github.com>
2024-02-28 20:35:52 +0700

    create Main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import { RouterProvider } from "react-router-dom";
 import AuthProvider from "./context/AuthProvider.jsx";
 import router from "./router/Router.jsx";
 import "./index.css";
-
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>

209ef2a40321c04cdfb5e7bcc573ad7245a66476
dacdepchai <93912537+dacdepchai@users.noreply.github.com>
2024-03-03 23:30:26 +0700

    update css link

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import AuthProvider from "./context/AuthProvider.jsx";
 import router from "./router/Router.jsx";
-import "./index.css";
+import "./App.css";
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>

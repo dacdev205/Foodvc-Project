b46f6c7a21867ee6c0c50f5f0383bbb595226206
dacdepchai <93912537+dacdepchai@users.noreply.github.com>
2024-03-06 12:08:07 +0700

    config router

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,13 +8,19 @@ export default defineConfig({
     port: 3001,
     proxy: {
       "/api/foodvc": {
-      target: "https://foodvc-server.onrender.com",
-      changeOrigin: true,
+        target: "https://foodvc-server.onrender.com",
+        changeOrigin: true,
       },
       "/inventory": {
         target: "https://foodvc-server.onrender.com",
         changeOrigin: true,
-        },
       }
+    },
+    // Thêm rewrite rules vào đây
+    // Đảm bảo các request không khớp với route nào sẽ được chuyển hướng đến index.html
+    fs: {
+      strict: true,
+    },
+    historyApiFallback: true, // Chấp nhận routing client-side
   },
-})
+});

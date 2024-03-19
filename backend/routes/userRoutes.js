eb8d566acc70397da8598abe0f1e8e507533a3d2
dacdepchai <93912537+dacdepchai@users.noreply.github.com>
2024-03-19 23:13:33 +0700

    create getUserByEmail router

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,6 +7,7 @@ const verifyAdmin = require('../middleware/verifyAdmin')
 router.get("/",verifyToken, verifyAdmin, usersAPI.getAllUsers);
 router.post("/", usersAPI.createUser);
 router.get("/:id", usersAPI.getSigleUser)
+router.get("/getUserByEmail/:email",usersAPI.getUserByEmail)
 router.delete("/:id",verifyToken, verifyAdmin,usersAPI.deleteUser);
 router.get("/getAdmin/:email",verifyToken, usersAPI.getAdmin);
 router.patch("/makeAdmin/:id",verifyToken, verifyAdmin,usersAPI.makeAdmin)

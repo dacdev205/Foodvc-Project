63eaffda4c9f188d8fdb6f5be2cdcc7f318e1f1f
dacdepchai <93912537+dacdepchai@users.noreply.github.com>
2024-03-19 23:10:13 +0700

    create new api

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.use("/check-out", require("./routes/paymentRoutes"))
 app.use("/adminStats",require("./routes/adminStats"))
 app.use("/order", require("./routes/ordersRoutes"))
 app.use("/address", require("./routes/addressRoutes"))
-
+app.use('/api/messages', require("./routes/messageRoutes"));
 app.post('/jwt', async(req, res) => {
     const user = req.body;
     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {

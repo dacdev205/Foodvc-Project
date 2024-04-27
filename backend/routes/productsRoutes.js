b2d2bbfa1196d384adcc4400c28ae74edf716b1b
dacdepchai <93912537+dacdepchai@users.noreply.github.com>
2024-04-27 21:57:57 +0700

    add new Routes

---
+++ backend/routes/productsRoutes.js
@@ -0,0 +1,20 @@
+const productsAPI = require("../controllers/productsControllers");
+const router = require("express").Router();
+const multer = require("multer");
+//multer middleware
+let storage = multer.diskStorage({
+    destination: function (req, file,cb) {
+        cb(null, "uploads");
+    },
+    filename: function (req, file,cb) {
+        cb(null,file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    },
+});
+
+let upload = multer({
+    storage: storage,
+}).single("image")
+
+router.patch("/:id",upload, productsAPI.updateProduct);
+
+module.exports = router;

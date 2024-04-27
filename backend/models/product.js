996a208f1a7ad684e0786b1e6e6ab8386a4cfc19
dacdepchai <93912537+dacdepchai@users.noreply.github.com>
2024-04-27 21:57:30 +0700

    add new model

---
+++ backend/models/product.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    minlength: 3
+  },
+  recipe: String,
+  image: String,
+  brand: String,  
+  category: String,
+  price: Number,
+  quantity: Number,
+  productionLocation: String,  
+  instructions: String,  
+  expirationDate: {
+    type: Date,
+    default: function() {
+      const now = new Date();
+      now.setDate(now.getDate() + 4);
+      return now;
+    }
+  },
+  transferredToMenu: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);

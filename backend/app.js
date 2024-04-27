0136a2f59d7386829aeef9f0f02c0f3c1b78d0d8
dacdepchai <93912537+dacdepchai@users.noreply.github.com>
2024-04-27 21:16:59 +0700

    add new endpoint

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ app.use("/order", require("./routes/ordersRoutes"))
 app.use("/address", require("./routes/addressRoutes"))
 app.use('/api/conversations', require("./routes/conversationRoutes"));
 app.use('/api/messages', require("./routes/messageRoutes"));
-
+app.use("/products", require("./routes/productsRoutes"));
 app.post('/jwt', async(req, res) => {
     const user = req.body;
     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {

652c6ff2b7cb0bb006d6c00a0debd135d3f4129d
dacdev205 <datdev252@gmail.com>
2024-05-31 23:01:37 +0700

    config file

--- frontend/src/pages/dashboard/admin/AddVoucher.jsx
+++ frontend/src/pages/dashboard/admin/AddVoucher.jsx
@@ -1,72 +1,201 @@
 import React, { useState, useEffect } from "react";
-import voucherAPI from "../../../api/voucherAPI";
-// import { format } from "date-fns";
-import AddVoucherModal from "../../../components/AddVoucherModal";
-
+import useMenu from "../../../hooks/useMenu";
+import Pagination from "../../../ultis/Pagination";
+import axios from "axios";
+import inventoryAPI from "../../../api/inventoryAPI";
+import FormattedPrice from "../../../ultis/FormatedPriece";
+import { FaCheck } from "react-icons/fa6";
 const AddVoucher = () => {
+  const [menu, , refetch] = useMenu();
   const PF = "http://localhost:3000";
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortType, setSortType] = useState("name");
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(5);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [discountPercentage, setDiscountPercentage] = useState("");
+  const categories = [...new Set(menu.map((item) => item.category))];
+  const [selectedProducts, setSelectedProducts] = useState([]);
 
-  const [voucher, setVoucher] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  useEffect(() => {
-    const fetchVoucherList = async () => {
-      try {
-        const data = await voucherAPI.getAllVoucher();
-        setVoucher(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchVoucherList();
-  }, []);
-
-  const formatDateTime = (dateTimeString) => {
-    const options = {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    };
-    const dateTime = new Date(dateTimeString);
-    return dateTime.toLocaleDateString("vi-VN", options);
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
   };
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  useEffect(() => {
+    refetch();
+  }, [sortType, sortOrder, refetch]);
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+    setCurrentPage(1);
+  };
+
+  const handleSort = (type) => {
+    if (type === sortType) {
+      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    } else {
+      setSortType(type);
+      setSortOrder("asc");
+    }
+  };
+  const filteredMenu = menu.filter((item) => {
+    const nameMatches = item.name
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+    const categoryMatches =
+      selectedCategory === "" || item.category === selectedCategory;
+    return nameMatches && categoryMatches;
+  });
+
+  const sortedMenu = filteredMenu.sort((a, b) => {
+    const dateA = new Date(a.createdAt);
+    const dateB = new Date(b.createdAt);
+
+    return sortOrder === "desc" ? dateA - dateB : dateB - dateA;
+  });
+  const currentMenu = sortedMenu.slice(indexOfFirstItem, indexOfLastItem);
+
+  const handleApplyVoucher = async () => {
+    try {
+      // Validate inputs
+      if (selectedProducts.length === 0 || !discountPercentage) {
+        alert("Vui lòng chọn ít nhất một sản phẩm và nhập thông tin voucher.");
+        return;
+      }
+      // Send a request to the server to apply voucher for selected products
+      const response = await axios.post("/api/foodvc/apply-voucher", {
+        productId: selectedProducts,
+        discount: parseFloat(discountPercentage),
+      }); // Update the API path accordingly
+      await inventoryAPI.updateProduct(selectedProducts, {
+        applyVoucher: true,
+      });
+      if (response.data.success) {
+        alert("Voucher added succesfully");
+      } else {
+        console.error("Error applying voucher:", response.data.error);
+      }
+    } catch (error) {
+      console.error("Error applying voucher:", error);
+    }
+    refetch();
+  };
+
+  const handleCheckboxChange = (e, productId) => {
+    const isChecked = e.target.checked;
+    if (isChecked) {
+      setSelectedProducts((prevSelectedProducts) => [
+        ...prevSelectedProducts,
+        productId,
+      ]);
+    } else {
+      setSelectedProducts((prevSelectedProducts) =>
+        prevSelectedProducts.filter((id) => id !== productId)
+      );
+    }
+  };
+  const mapCategoryToDisplayName = (category) => {
+    switch (category) {
+      case "protein":
+        return "Thịt, cá, trứng, hải sản";
+      case "vegetable":
+        return "Rau củ, nấm, trái cây";
+      case "soup":
+        return "Mì, miến, cháo, phở";
+      case "milk":
+        return "Sữa các loại";
+      case "drinks":
+        return "Bia, nước giải khát";
+      default:
+        return category;
+    }
+  };
+
   return (
-    <div className="w-full px-4 mx-auto">
+    <div className="w-full md:w-[870px] px-4 mx-auto">
       <h2 className="text-2xl font-semibold my-4 text-black">
-        Quản lý khuyến mãi <span className="text-green">sản phẩm</span>
+        Thêm giảm giá cho <span className="text-green">sản phẩm</span>
       </h2>
       <div>
         <div className="flex items-center">
-          <button
-            onClick={() => setIsModalOpen(true)}
-            className="btn btn-ghost bg-green text-white ml-10 hover:bg-green hover:opacity-80"
+          <label className="text-black">Loại Hàng/Mặt Hàng:</label>
+          <select
+            value={selectedCategory}
+            onChange={(e) => setSelectedCategory(e.target.value)}
+            className="select-sm"
           >
-            Thêm mới Voucher
+            <option value="">-- Chọn loại --</option>
+            {categories.map((category) => (
+              <option key={category} value={category}>
+                {mapCategoryToDisplayName(category)}
+              </option>
+            ))}
+          </select>
+          <div className="flex items-center my-2 ml-5">
+            <label htmlFor="search" className="mr-2 text-black">
+              Tìm kiếm theo tên:
+            </label>
+            <input
+              type="text"
+              id="search"
+              placeholder="Nhập tên sản phẩm"
+              value={searchTerm}
+              onChange={handleSearch}
+              className="border p-2 rounded-md text-black input-sm"
+            />
+          </div>
+        </div>
+        <div className="flex items-center">
+          <div>
+            <label className="text-black">Phần Trăm Giảm Giá:</label>
+            <input
+              type="number"
+              className="border p-2 rounded-md text-black input-sm"
+              placeholder="VD: 5"
+              value={discountPercentage}
+              onChange={(e) => {
+                const inputValue = e.target.value;
+                if (inputValue >= 0 && inputValue <= 100) {
+                  setDiscountPercentage(inputValue);
+                }
+              }}
+            />
+            <span className="ml-1 text-black">%</span>
+          </div>
+          <button
+            className="btn btn-ghost bg-green text-white ml-10 hover:bg-green hover:opacity-80"
+            onClick={handleApplyVoucher}
+          >
+            Áp Dụng Voucher
           </button>
-          <AddVoucherModal
-            isModalOpen={isModalOpen}
-            setIsModalOpen={setIsModalOpen}
-          />
         </div>
       </div>
 
       <div>
         <div className="overflow-x-auto">
-          <table className="table w-full">
+          <table className="table ">
             <thead>
               <tr className="text-black border-style">
                 <th>#</th>
                 <th>Stt</th>
-                <th className="cursor">Mã voucher</th>
-                <th className="cursor">Mô tả voucher</th>
-                <th>Trạng thái</th>
-                <th>% Giảm</th>
-                <th>Ngày hết hạn</th>
-                <th className="text-center">Số lượng</th>
+                <th>Hình ảnh</th>
+                <th onClick={() => handleSort("name")} className="curser">
+                  Tên sản phẩm
+                </th>
+                <th onClick={() => handleSort("price")}>Giá</th>
+                <th
+                  onClick={() => handleSort("quantity")}
+                  className="text-center"
+                >
+                  Số lượng
+                </th>
               </tr>
             </thead>
             <tbody>
-              {voucher.map((item, index) => (
+              {currentMenu.map((item, index) => (
                 <tr key={index} className="text-black border-gray-300">
                   <td>
                     <label
@@ -75,23 +204,45 @@ const AddVoucher = () => {
                     >
                       <input
                         type="checkbox"
+                        id={`checkbox-${item._id}`}
+                        checked={selectedProducts.includes(item._id)}
+                        onChange={(e) => handleCheckboxChange(e, item._id)}
                         className="appearance-none w-4 h-4 rounded-sm bg-white border-2 border-[#39d84A] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                      />
+                      <FaCheck
+                        className={`absolute top-0 left-[1px] text-green ${
+                          selectedProducts.includes(item._id)
+                            ? "text-opacity-100"
+                            : "text-opacity-0"
+                        } check-${item._id} transition`}
                       />
                     </label>
                   </td>
                   <th>{index + 1}</th>
-                  <td>{item.code}</td>
-                  <td>{item.voucher_describe.slice(0, 20)}...</td>
-                  <td>{item.status ? "Hoạt động" : "Không hoạt động"}</td>
-                  <td className="text-center">
-                    {item.voucher_discount_persent} %
+
+                  <td>
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img src={PF + "/" + item.image} alt="" />
+                      </div>
+                    </div>
                   </td>
-                  <td>{formatDateTime(item.voucher_experied_date)}</td>
+                  <td>{item.name.slice(0, 20)}...</td>
+                  <td>
+                    <FormattedPrice price={item.price} />
+                  </td>
                   <td className="text-center">{item.quantity}</td>
                 </tr>
               ))}
             </tbody>
           </table>
+
+          <Pagination
+            itemsPerPage={itemsPerPage}
+            totalItems={filteredMenu.length}
+            currentPage={currentPage}
+            paginate={paginate}
+          />
         </div>
       </div>
     </div>

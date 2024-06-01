29a209f9c34ffda1ee1fc4ead20cee941fabc5bc
dacdev205 <datdev252@gmail.com>
2024-06-01 16:21:00 +0700

    add new ultis alert

---
+++ frontend/src/ultis/SuccessAlert.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Bounce, ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+
+// eslint-disable-next-line react/prop-types
+const SuccessAlert = ({ show, message }) => {
+  if (!show) {
+    return null;
+  }
+  const notify = () =>
+    toast.success(message, {
+      position: "bottom-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      transition: Bounce,
+    });
+
+  return (
+    <>
+      {show && (
+        <div className="fixed top-4 right-4 z-50">
+          {notify()}
+          <ToastContainer
+            position="bottom-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+            transition:Bounce
+          />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default SuccessAlert;

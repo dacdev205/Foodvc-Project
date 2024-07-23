310307f6c9d196894d9f30b8a8f1f09bbedb43cd
dacdev205 <datdev252@gmail.com>
2024-07-23 14:59:21 +0700

    config files

--- frontend/src/pages/dashboard/admin/UpdateItem.jsx
+++ frontend/src/pages/dashboard/admin/UpdateItem.jsx
@@ -7,9 +7,6 @@ import React, { useEffect, useState } from "react";
 import QuillEditor from "../../../ultis/QuillEditor";
 import productsAPI from "../../../api/productsAPI";
 import SuccessAlert from "../../../ultis/SuccessAlert";
-import Button from "@mui/material/Button";
-import CloudUploadIcon from "@mui/icons-material/CloudUpload";
-import { styled } from "@mui/material";
 const UpdateItem = () => {
   const { register, handleSubmit, setValue } = useForm({ mode: "onChange" });
   const [product, setProduct] = useState(null);
@@ -29,17 +26,6 @@ const UpdateItem = () => {
 
     fetchProductDetail();
   }, [id]);
-  const VisuallyHiddenInput = styled("input")({
-    clip: "rect(0 0 0 0)",
-    clipPath: "inset(50%)",
-    height: 1,
-    overflow: "hidden",
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    whiteSpace: "nowrap",
-    width: 1,
-  });
   const onSubmit = async (data) => {
     try {
       const formData = new FormData();
--- frontend/src/ultis/SuccessAlert.jsx
+++ frontend/src/ultis/SuccessAlert.jsx
@@ -1,46 +1,41 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import React, { useEffect } from "react";
 import { Bounce, ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 
-// eslint-disable-next-line react/prop-types
 const SuccessAlert = ({ show, message }) => {
-  if (!show) {
-    return null;
-  }
-  const notify = () =>
-    toast.success(message, {
-      position: "bottom-right",
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "colored",
-      transition: Bounce,
-    });
+  useEffect(() => {
+    if (show) {
+      toast.success(message, {
+        position: "bottom-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        transition: Bounce,
+      });
+    }
+  }, [show, message]);
 
   return (
-    <>
-      {show && (
-        <div className="fixed top-4 right-4 z-50">
-          {notify()}
-          <ToastContainer
-            position="bottom-right"
-            autoClose={3000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="colored"
-            transition:Bounce
-          />
-        </div>
-      )}
-    </>
+    <div className="fixed top-4 right-4 z-50">
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        transition={Bounce}
+      />
+    </div>
   );
 };
 

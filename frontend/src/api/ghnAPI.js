f1d08de5704fdbab8526629eb41ced59cb20781d
dacdev205 <datdev252@gmail.com>
2024-07-30 21:41:48 +0700

    add GHN api

---
+++ frontend/src/api/ghnAPI.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+const url = "https://dev-online-gateway.ghn.vn/shiip/public-api/v2";
+const GHN_TOKEN = import.meta.env.VITE_GHN_TOKEN;
+
+export default class ghnAPI {
+  static async getAddressFOODVC(
+    offset = 0,
+    limit = 50,
+    clientPhone = "0962034466"
+  ) {
+    try {
+      const response = await axios.post(
+        `${url}/shop/all`,
+        {
+          offset: offset,
+          limit: limit,
+          client_phone: clientPhone,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Token: GHN_TOKEN,
+          },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.log(error.message);
+      throw error;
+    }
+  }
+  static async createOrder(payload) {
+    try {
+      const response = await axios.post(
+        `${url}/shipping-order/create`,
+        payload,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Token: GHN_TOKEN,
+          },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.log(error.message);
+      throw error;
+    }
+  }
+}

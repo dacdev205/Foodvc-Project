20b0a076b8e714f75cf139756df334f1127eb2b5
dacdev205 <datdev252@gmail.com>
2024-11-10 11:21:22 +0700

    prefix hook

--- frontend/src/hooks/usePermission.jsx
+++ frontend/src/hooks/usePermission.jsx
@@ -2,32 +2,36 @@ import useAuth from "./useAuth";
 import useAxiosSecure from "./useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 
-const usePermission = (permission) => {
+const usePermission = (permissionName) => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
   const {
     refetch,
-    data: hasPermission,
+    data: permissions,
     isLoading: isPermissionLoading,
   } = useQuery({
-    queryKey: [user?.email, "permissions", permission],
+    queryKey: [user?.email, "permissions"],
     queryFn: async () => {
       if (!user || !user.email) {
         console.error("User or user email is undefined");
-        return false;
+        return [];
       }
 
       try {
         const res = await axiosSecure.get(`users/getPermissions/${user.email}`);
-        const permissions = res.data?.permissions || [];
-        return permissions.includes(permission);
+        const userPermissions = res.data?.permissions || [];
+        return userPermissions;
       } catch (error) {
         console.error("Error fetching permissions", error);
-        return false;
+        return [];
       }
     },
   });
+
+  const hasPermission = permissions?.some(
+    (permission) => permission.name === permissionName
+  );
 
   return [hasPermission, isPermissionLoading, refetch];
 };
